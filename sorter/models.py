27abd9cf060043e8bde1720071d2c344c47d4b09
chapkovski <chapkovski@gmail.com>
2018-10-27 09:29:32 +0300

    extra check of code correct

--- sorter/models.py
+++ sorter/models.py
@@ -3,8 +3,7 @@ from otree.api import (
     Currency as c, currency_range
 )
 import csv
-import json
-import random
+from string import digits
 
 author = 'Philipp Chapkovski, HSE, Moscow, chapkovski@gmail.com'
 
@@ -14,6 +13,7 @@ A single-player, single-page app collecting ids for players from several cities.
 
 NO_CITY_REGISTERED_MSG = '{}: No such city in cities list'
 BOTH_SORTING_OPTS_MSG = 'You cannot choose both hetero and homo sorting'
+WRONG_CITY_CODE_MSG = 'This city code ({}) is wrong. Check it out!'
 
 
 class Constants(BaseConstants):
@@ -24,16 +24,17 @@ class Constants(BaseConstants):
     with open('sorter/cities.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         cities = {row['code']: row['city'] for row in reader}
+    for k in cities.keys():
+        l = list(k)
+        assert all(i in digits for i in l) and len(l) == 2, WRONG_CITY_CODE_MSG.format(k)
 
 
 class Subsession(BaseSubsession):
     def creating_session(self):
-        for p in self.get_players():
-            p.participant.vars['group_id'] = random.choice([1, 2])
         homo, hetero = self.session.config['homo'], self.session.config['hetero']
         assert not all([homo, hetero]), BOTH_SORTING_OPTS_MSG
         config_cities = [v for k, v in self.session.config.items() if
-                         k.startswith('city_') and v.strip()  not in (None, '')]
+                         k.startswith('city_') and v.strip() not in (None, '')]
         for c in config_cities:
             assert c in Constants.cities, NO_CITY_REGISTERED_MSG.format(c)
         self.session.vars['cities_list'] = list(set(config_cities))
